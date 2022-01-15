0ee2e6a333b3edfd08ade9d1afe920a029fa38e9
Saul <sulayman.jawneh.@qcell.gm>
2022-01-15 06:41:00 +0000

    directory changes

--- backend/server.js
+++ backend/server.js
@@ -50,9 +50,9 @@ app.use(errorHandler) //handles all errors and error response
 // const __dirname = path.resolve()
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")))
-  app.get("/", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  app.use(express.static(path.resolve(__dirname, "../frontend/build")))
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"))
   )
 } else {
   app.get("/", (req, res) => {
