1daa8c80df6ba6c2bc4275831c08321bb9e8682b
Saul <sulayman.jawneh.@qcell.gm>
2022-01-15 06:59:45 +0000

    make it better

--- backend/server.js
+++ backend/server.js
@@ -52,7 +52,7 @@ app.use(errorHandler) //handles all errors and error response
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.resolve(__dirname, "../frontend/build")))
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   )
 } else {
   app.get("/", (req, res) => {
