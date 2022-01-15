b8eee1cdc905ebac1fd733e02830e2676b70b865
Saul <sulayman.jawneh.@qcell.gm>
2022-01-15 06:10:01 +0000

    add path asterisk all

--- backend/server.js
+++ backend/server.js
@@ -51,7 +51,7 @@ app.use(errorHandler) //handles all errors and error response
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")))
-  app.get("*", (req, res) =>
+  app.get("/*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   )
 } else {
