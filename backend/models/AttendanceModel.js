a266ef708209ff06b801478f9f0a56ddb1df6d51
Saul <sulayman.jawneh.@qcell.gm>
2022-01-19 22:23:21 +0000

    bug

--- backend/models/AttendanceModel.js
+++ backend/models/AttendanceModel.js
@@ -6,7 +6,7 @@ const AttendanceSchema = new Schema(
     attendees: [{ type: Schema.Types.ObjectId, ref: "users" }],
     course_id: { type: Schema.Types.ObjectId, ref: "courses", required: true },
     end_time: { type: String, required: true },
-    expires: { type: Date, required: true },
+    expiry: { type: Date, required: true },
     latitude: { type: String, required: false },
     longitude: { type: String, required: false },
     qr_code: { type: String },
